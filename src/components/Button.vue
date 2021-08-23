78f93ef019c9c2a770b9a604e994790f825abe55
Caelan Curry <caelan.curry@gmail.com>
2021-08-23 16:20:01 +0200

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="toggleTasks()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    toggleTasks() {
+      this.$emit("btn-click");
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
